6097f3e923bcaa1e6af72e100f3c20f35352ade0
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-03-10 10:05:46 +0900

    Initial commit

---
+++ woo-dong-spring/src/main/java/woodong/woodongspring/WooDongSpringApplication.java
@@ -0,0 +1,13 @@
+package woodong.woodongspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooDongSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooDongSpringApplication.class, args);
+	}
+
+}
---
+++ woo-dong-spring/src/test/java/woodong/woodongspring/WooDongSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package woodong.woodongspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooDongSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
